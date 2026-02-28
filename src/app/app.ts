1fa5615667b82ac10ba7717866cf62e4240099b5
Akshata Shrivastava <akshataps75@gmail.com>
2026-02-28 18:32:08 +0530

    Added time progress and right or wrong answer feedback

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 //an interface forces the object to follow a specific structure
 interface Question {
@@ -15,16 +15,26 @@ interface Question {
   styleUrl: './app.css'
 })
 
-
 export class App {
   //define  the three state variables
-  // : below is type annotation (defining what all the variable currentStep can be)
+  // : below is type annotation (defining what all the variable can be)
   //pipe creates a union type 
   currentStep : 'start' | 'quiz' | 'results' = 'start';
 
   //track the questions
   currentQuestion = 0;
   score = 0;
+
+  //timer variables
+  timeLeft = 60;
+  timerInterval: any;
+
+  // Add constructor to use Change Detection manually if needed
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  //correct or wrong answers
+  isAnswered = false;
+  selectedOption: number | null = null;
 
   //a typed array
   questions: Question[] = [
@@ -61,20 +71,56 @@ export class App {
     //to ensure current question and score are always 0 when quiz starts
     this.currentQuestion = 0;
     this.score = 0;
+    this.startTimer();
   }
 
   //handle answer
   handleAnswer(optionIndex: number) {
+    //check if answer is clicked
+    if (this.isAnswered) return;
+    
+    this.isAnswered = true;
+    this.selectedOption = optionIndex;
+    clearInterval(this.timerInterval); //stop the timer
+
     //if correct
     if (optionIndex === this.questions[this.currentQuestion].correctAnswer) {
       this.score++;
     }
+    //wait for user to see the feedback then move on to next question
+    setTimeout(() => {
+      this.goToNextQuestion();
+    }, 1500);
+  }
 
-    //move to next question or show result if all questions are over
-    if(this.currentQuestion < this.questions.length - 1) {
+  startTimer(){
+    this.timeLeft = 60;
+    clearInterval(this.timerInterval);
+
+    this.timerInterval = setInterval(()=> {
+      if (this.timeLeft > 0){
+        this.timeLeft--;
+        this.cdr.detectChanges();
+      } else {
+        this.goToNextQuestion();
+      }
+    }, 1000);
+  }
+
+  goToNextQuestion(){
+    // Reset the feedback state for the new question
+    this.isAnswered = false;
+    this.selectedOption = null;
+
+    clearInterval(this.timerInterval);
+    if (this.currentQuestion < this.questions.length - 1) {
       this.currentQuestion++;
+      this.timeLeft = 60;
+      this.startTimer();
+      this.cdr.detectChanges(); // Refresh UI for new question
     } else {
       this.currentStep = 'results';
+      this.cdr.detectChanges();
     }
   }
 
